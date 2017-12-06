c7d194edafea8c0f01816f649b730d29bbd08a02
Amit Kshirsagar <amit.kshirsagar.13@gmail.com>
2017-12-06 21:28:38 +0530

    Working everything including swagger

---
+++ course-catagory/src/main/java/io/lms/ResourceServerConfig.java
@@ -0,0 +1,35 @@
+package io.lms;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+import io.lms.security.BaseResourceServerConfig;
+
+/**
+ * 
+ * <p>
+ * <b>Overview:</b>
+ * <p>
+ * 
+ * 
+ * <pre>
+ * &#64;projectName base-service
+ * Creation date: Dec 6, 2017
+ * &#64;author Amit Kshirsagar
+ * &#64;version 1.0
+ * &#64;since
+ * 
+ * <p><b>Modification History:</b><p>
+ * 
+ * 
+ * </pre>
+ */
+@Configuration
+@EnableResourceServer
+@EnableWebSecurity
+@EnableConfigurationProperties
+public class ResourceServerConfig extends BaseResourceServerConfig {
+
+}
